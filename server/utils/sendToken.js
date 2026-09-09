867ba1e57458c4d24c1d41952cdb43d9c062c2f1
YungD517 <fawazyunusayoola@gmail.com>
2026-09-09 10:00:48 +0100

    still on launch

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -10,7 +10,7 @@ const sendToken = (user, statusCode, res) => {
       Date.now() + parseInt(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     secure: process.env.NODE_ENV === "production",
   };
 
