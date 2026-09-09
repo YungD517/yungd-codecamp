dee4e361bd7ac0e2742e0a368d0a8711000a7186
YungD517 <fawazyunusayoola@gmail.com>
2026-09-09 09:43:50 +0100

    Still fixing netlify launch

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
-  withCredentials: true,
+  baseURL: "https://yungd-codecamp-api.onrender.com/api", withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
